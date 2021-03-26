afe1740b29f1709b219e08323e9fd5c6b18b8479
asaini <abhi@streamlit.io>
2021-03-26 12:21:39 -0700

    Updates to language

--- app.py
+++ app.py
@@ -13,28 +13,15 @@ With Anchor Tags, we can now create a Table of contents which links to other sec
 
 **Table of Contents**
 
- - [Demo](#demo)
+ - [Demo](#demo-for-anchor-tags)
     - [Section 1](#section-1)
         - [Section 1a](#section-1a)
         - [Section 1b](#section-1b)
     - [Section 2](#section-2-multiple-columns)
-        - [Section 2a](#section-2a)
         - [Section 2b](#section-2b)
 ''')
 
-st.title('This is a demo for Anchor Tags')
-
-col1, col2 = st.beta_columns(2)
-
-with col1:
-    st.image('https://stmedia.stimg.co/ows_157235893649127.jpg?auto=compress&crop=faces&dpr=2&w=230')
-
-with col2:
-    st.subheader('On the left is a bird image')
-    st.markdown('''There seems to be some overlap between the image and the anchor tag. 
-    The tag is visible and discernable in Light mode. 
-    Not so much in Dark Mode. This behavior can be seen in other tags down below as well.
-    ''')
+st.title('Demo for Anchor Tags')
 
 
 st.markdown('---')
@@ -55,16 +42,35 @@ st.markdown('''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
 Maecenas blandit sapien odio, id bibendum leo consectetur vitae. Donec tempus venenatis lorem, in porta felis malesuada eget. Aenean suscipit commodo ligula. Nunc consequat volutpat mauris, ut luctus orci viverra in. Morbi sed blandit tortor. Donec at lacinia nisl, at eleifend urna. Nullam nec ligula vulputate ligula blandit ornare non in lectus. Proin blandit suscipit nisi, sed pellentesque libero suscipit in. Vivamus luctus augue augue. Vivamus consequat, nunc vitae finibus ultricies, nunc dui auctor justo, ut tincidunt ipsum enim a nibh. Donec convallis interdum diam, rhoncus porta ante bibendum a. Duis efficitur convallis condimentum.''')
 
 st.header('Section 2 Multiple Columns')
-st.markdown('''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel dolor convallis, rhoncus elit a, tempus nulla. Aliquam erat volutpat. In turpis erat, fermentum nec ullamcorper nec, egestas a nibh. Nam pharetra nunc vitae sem pellentesque, vel convallis nibh convallis. Duis mattis egestas suscipit. Donec ligula mauris, lacinia ut mattis eget, blandit non ipsum. Sed lobortis semper blandit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam orci orci, porttitor vitae malesuada sit amet, pretium vel ex. Praesent quis purus ut libero dapibus dignissim. Sed tempor volutpat risus id sollicitudin. Nunc tincidunt velit elit, non ultrices tortor consectetur non. Mauris vitae molestie magna. Donec vitae hendrerit tortor, vel ultrices diam.
-Maecenas blandit sapien odio, id bibendum leo consectetur vitae. Donec tempus venenatis lorem, in porta felis malesuada eget. Aenean suscipit commodo ligula. Nunc consequat volutpat mauris, ut luctus orci viverra in. Morbi sed blandit tortor. Donec at lacinia nisl, at eleifend urna. Nullam nec ligula vulputate ligula blandit ornare non in lectus. Proin blandit suscipit nisi, sed pellentesque libero suscipit in. Vivamus luctus augue augue. Vivamus consequat, nunc vitae finibus ultricies, nunc dui auctor justo, ut tincidunt ipsum enim a nibh. Donec convallis interdum diam, rhoncus porta ante bibendum a. Duis efficitur convallis condimentum.''')
+st.markdown('''
+In this section of the app we focus on how anchors get rendered in multiple columns
+''')
+
+col1, col2 = st.beta_columns(2)
+
+with col1:
+    st.image('https://stmedia.stimg.co/ows_157235893649127.jpg?auto=compress&crop=faces&dpr=2&w=230')
+
+with col2:
+    st.subheader('On the left is a bird image')
+    st.markdown('''There seems to be some overlap between the image and the anchor tag. 
+* The tag is visible and discernable in Light mode. 
+* Not so much in Dark Mode. This behavior can be seen in other tags down below as well.
+
+    ''')
+
 
 col1, col2 = st.beta_columns(2)
 
 with col2:
-    st.subheader('Section 2a')
-    st.markdown('''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel dolor convallis, rhoncus elit a, tempus nulla. Aliquam erat volutpat. In turpis erat, fermentum nec ullamcorper nec, egestas a nibh. Nam pharetra nunc vitae sem pellentesque, vel convallis nibh convallis. Duis mattis egestas suscipit. Donec ligula mauris, lacinia ut mattis eget, blandit non ipsum. Sed lobortis semper blandit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam orci orci, porttitor vitae malesuada sit amet, pretium vel ex. Praesent quis purus ut libero dapibus dignissim. Sed tempor volutpat risus id sollicitudin. Nunc tincidunt velit elit, non ultrices tortor consectetur non. Mauris vitae molestie magna. Donec vitae hendrerit tortor, vel ultrices diam.
-Maecenas blandit sapien odio, id bibendum leo consectetur vitae. Donec tempus venenatis lorem, in porta felis malesuada eget. Aenean suscipit commodo ligula. Nunc consequat volutpat mauris, ut luctus orci viverra in. Morbi sed blandit tortor. Donec at lacinia nisl, at eleifend urna. Nullam nec ligula vulputate ligula blandit ornare non in lectus. Proin blandit suscipit nisi, sed pellentesque libero suscipit in. Vivamus luctus augue augue. Vivamus consequat, nunc vitae finibus ultricies, nunc dui auctor justo, ut tincidunt ipsum enim a nibh. Donec convallis interdum diam, rhoncus porta ante bibendum a. Duis efficitur convallis condimentum.''')
+    st.subheader('Section 2b')
+    st.markdown('''
+    On the left column is a plotly chart. 
 
+We can see that there is no overlap between the chart supporting 
+icons and the anchor tags icon. Moreover, both appear to only appear on hover so there is no confusion in 
+distinguishing between them.
+    ''')
 with col1:
     # st.subheader('Section 2b')
     x1 = np.random.randn(200) - 2
